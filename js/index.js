11df955c2eefe06b1d46da335bf55ac9d6050ad8
Allset <133010630+DeveloperFlow@users.noreply.github.com>
2023-08-22 12:33:07 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,16 +5,21 @@
 
     rightSlide.onclick = function(){MoveSlider(true)}
     leftSlide.onclick = function(){MoveSlider(false)}
+    slider.onscroll = checkSlideEnd
     function MoveSlider(dir){
-        by = slider.children[0].clientWidth
+        by = slider.children[0].clientWidth * Math.floor(slider.clientWidth / slider.children[0].clientWidth)
         if(!dir){by *= -1}
-        slider.scrollBy(by,0)
+        var leftScroll = slider.scrollLeft
+        slider.scroll({top:0,left:leftScroll + by,behavior:"smooth"})
+    }
+    function checkSlideEnd(){
+        var leftScroll = slider.scrollLeft
         var totalScroll = slider.scrollWidth - slider.clientWidth
-        if(slider.scrollLeft >= totalScroll){
+        if(leftScroll >= totalScroll){
             rightSlide.disabled = true
         }
         else{rightSlide.disabled = false}
-        if(slider.scrollLeft <= 0){
+        if(leftScroll <= 0){
             leftSlide.disabled = true
         }
         else{leftSlide.disabled = false}
