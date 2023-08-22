a3675a9cf866ec3e68817d4bcb86fb5425af7066
Allset <133010630+DeveloperFlow@users.noreply.github.com>
2023-08-22 10:15:14 +0100

    Update index.js
    Updated the slider to slide per first child's width in pixels

--- js/index.js
+++ js/index.js
@@ -3,9 +3,11 @@
     var rightSlide = document.getElementById("right-slide")
     var slider = document.getElementById("trending-now-slider")
 
-    rightSlide.onclick = function(){MoveSlider(400)}
-    leftSlide.onclick = function(){MoveSlider(-400)}
-    function MoveSlider(by){
+    rightSlide.onclick = function(){MoveSlider(true)}
+    leftSlide.onclick = function(){MoveSlider(false)}
+    function MoveSlider(dir){
+        by = slider.children[0].clientWidth
+        if(!dir){by *= -1}
         slider.scrollBy(by,0)
         var totalScroll = slider.scrollWidth - slider.clientWidth
         if(slider.scrollLeft >= totalScroll){
